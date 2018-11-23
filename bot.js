3f0d102d727a0e90792a27d60c6ae7c8b886a1b0
vampory <44905458+vampory@users.noreply.github.com>
2018-11-23 13:25:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const Util = require('discord.js');
     client = new Discord.Client({sisableEveryone: true})
  const devs = ['432266600893120519','475070652727033858'];
-const adminprefix = "!";
+const adminprefix = "c";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
